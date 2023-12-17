c0869cb6cc8cfd5683b59cecf8047eae416ebf84
Laghrabi <laghrabichaimae@gmail.com>
2023-12-17 12:21:46 -0800

    py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,58 @@
 #!/usr/bin/python3
-""" Script that lists all states starting with N"""
 
+
+"""
+    Module that takes in an argument
+    displays all values in the states table of database
+    where name matches the argument
+"""
+
+
+import argparse
 import MySQLdb
-from sys import argv
+
+
+def filter_states(username, password, database, state_name):
+
+    try:
+        connection = MySQLdb.connect(
+                user=username,
+                passwd=password,
+                db=database,
+                host='localhost',
+                )
+
+        cursor = connection.cursor()
+
+        query = """ SELECT * FROM states
+          WHERE name LIKE '{}'
+          ORDER BY id ASC """.format(state_name,)
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        for name in result:
+            print(name)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        cursor.close()
+        connection.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            description='List all states from a MySQL database.')
+    parser.add_argument('username', type=str, help='MySQL username')
+    parser.add_argument('password', type=str, help='MySQL password')
+    parser.add_argument('database', type=str, help='Database name')
+    parser.add_argument('state_name', type=str, help='State name')
+    args = parser.parse_args()
+
+    filter_states(args.username, args.password, args.database, args.state_name)
+
 
 if __name__ == '__main__':
-
-    HOST = 'localhost'
-    PORT = 3306
-    MY_USER = argv[1]
-    MY_PSWD = argv[2]
-    MY_DB = argv[3]
-    NAME = argv[4]
-    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
-                         db=MY_DB, port=PORT)
-    cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(NAME)
-    cur.execute(query)
-    row_query = cur.fetchall()
-    for rq_print in row_query:
-        if rq_print[1] == NAME:
-            print(rq_print)
-    cur.close()
-    db.close()
+    main()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,58 @@
 #!/usr/bin/python3
-""" that takes in the name of a state as an argument and lists all """
 
+"""
+    Module that takes in the name of a state as an argument
+    list all cities of that state, using the database
+"""
+
+
+import argparse
 import MySQLdb
-from sys import argv
+
+
+def list_cities(username, password, database, state_name):
+
+    try:
+        connection = MySQLdb.connect(
+                user=username,
+                passwd=password,
+                db=database,
+                host='localhost',
+                port=3306)
+
+        cursor = connection.cursor()
+
+        query = '''SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC'''
+        cursor.execute(query, (state_name,))
+
+        result = cursor.fetchall()
+
+        city_name = [', '.join(name) for name in result]
+        print(', '.join(city_name))
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        cursor.close()
+        connection.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            description='List all states from a MySQL database.')
+    parser.add_argument('username', type=str, help='MySQL username')
+    parser.add_argument('password', type=str, help='MySQL password')
+    parser.add_argument('database', type=str, help='Database name')
+    parser.add_argument('state_name', type=str, help='State name')
+    args = parser.parse_args()
+
+    list_cities(args.username, args.password, args.database, args.state_name)
+
 
 if __name__ == '__main__':
-
-    HOST = 'localhost'
-    PORT = 3306
-    MY_USER = argv[1]
-    MY_PSWD = argv[2]
-    MY_DB = argv[3]
-    NAME = argv[4]
-    db = MySQLdb.connect(host=HOST, user=MY_USER, password=MY_PSWD,
-                         db=MY_DB, port=PORT)
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON \
-        cities.state_id = states.id WHERE states.name LIKE %s \
-        ORDER BY cities.id", (NAME,))
-    rows = cur.fetchall()
-    list = []
-    for r in rows:
-        list.append(r[0])
-    print(", ".join(list))
-    cur.close()
-    db.close()
+    main()
