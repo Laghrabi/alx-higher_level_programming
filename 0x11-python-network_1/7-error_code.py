eaa9552186b585752f2a089edefaad2ef8f5c65b
Laghrabi <laghrabichaimae@gmail.com>
2024-01-05 13:35:48 -0800

    py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,17 @@
-
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
+"""Display response body
 """
-import sys
+
 import requests
+from sys import argv
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
         print(r.text)
+    except:
+        print('Error code: {}'.format(r.status_code))
